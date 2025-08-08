7afcacb75572470b8275d735a927948f30f53945
hikariorigin <younao9103f@i.softbank.jp>
2025-08-08 14:58:39 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-// sw.js
-const CACHE_NAME = 'nl-cache-v2';
-const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'resonance-spots.geojson']; 
-// ← 'app.js' は外す
+// v3に上げて、app.jsはキャッシュしない
+const CACHE_NAME = 'nl-cache-v3';
+const ASSETS = ['.', 'index.html', 'manifest.webmanifest', 'resonance-spots.geojson'];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
